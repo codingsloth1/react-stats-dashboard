000728319fabf3f296803192199ae6d053f3c02c
codingsloth1 <aqsajaved681@gmail.com>
2026-03-25 13:45:50 +0500

    project side bar setup

---
+++ src/components/SideBar.jsx
@@ -0,0 +1,10 @@
+export default function SideBar(){
+    return(<div className="sidebar">
+        <h3 >DashBoard</h3>
+        <h3>Products</h3>
+    </div>
+        
+           
+        
+    )
+}

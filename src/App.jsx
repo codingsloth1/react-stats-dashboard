075b71053484d8b7d6a1ff543b58d63bb9bb4cf2
codingsloth1 <aqsajaved681@gmail.com>
2026-03-25 19:49:51 +0500

    setup card component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,22 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import SideBar from './components/SideBar'
+import Header from './components/Header'
+import Card from './components/Card'
 
 function App() {
   
 
   return (
     <>
-     <SideBar/> 
+     <SideBar/>  
+     <Header/>
+    <div className='card-wrapper'>
+     <Card title='Total Items'  number={285}/>
+     <Card title='Pending Items'  number={0}/>
+     <Card title='Completed Items'  number={122}/>
+     </div>
+   
     </>
   )
 }

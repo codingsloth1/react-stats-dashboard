c3fccc36788a124f65d4c0013cfe12b13f4bfa9b
codingsloth1 <aqsajaved681@gmail.com>
2026-03-26 13:54:24 +0500

    setup table component

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,21 @@ import './App.css'
 import SideBar from './components/SideBar'
 import Header from './components/Header'
 import Card from './components/Card'
+import Table from './components/Table'
 
 function App() {
   
 
   return (
     <>
-     <SideBar/>  
+      <SideBar/>  
      <Header/>
     <div className='card-wrapper'>
      <Card title='Total Items'  number={285}/>
      <Card title='Pending Items'  number={0}/>
      <Card title='Completed Items'  number={122}/>
-     </div>
-   
+     </div> 
+    <Table/>
     </>
   )
 }
